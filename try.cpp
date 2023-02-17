f575b66e735104d5d58453d219cd3bece7432f99
Akash Hegde <84635210+Akash-hegd@users.noreply.github.com>
2023-02-17 22:43:53 +0530

    Create try.cpp

---
+++ try.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully changed and commit\n";
+  cout << "This has been added";
+  return 0;
+}
